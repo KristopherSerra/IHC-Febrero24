01fe37e25b2887c02a4ddb2e930a84f391c83f72
Kristian <cdserra@alumnos.unsada.edu.ar>
2023-12-14 16:28:40 -0300

    modified:   Log in.html 	modified:   Sign in.html 	new file:   assets/CATEGORIAS.png 	deleted:    assets/Logo (1).jpg 	deleted:    assets/Logo (2).jpg 	deleted:    assets/Logo (3).jpg 	deleted:    assets/Logo (4).jpg 	deleted:    assets/Logo (5).jpg 	deleted:    assets/Logo (6).jpg 	deleted:    assets/Logo (7).jpg 	modified:   bd.php 	modified:   index.html 	modified:   style.css

--- bd.php
+++ bd.php
@@ -1,19 +1,23 @@
 <?php
-	$server = "localhost";
-    $user = "root";
-	$pass = "123abc";
-	$db = "db";
-	$connection = mysqli_connect($server, $user, $pass, $db)
-		or die("Conection Error");
+	$servername = "localhost";
+	$username = "root"
+	$password = "123456Ab_"
+	$database = "userdb";
 
-	$getdestinos = "select * from destinos";
-	$result = mysqli_query($connection, $getdestinos)
-
-		or die ("Insert Error");
-
-	while($row = mysqli_fetch_array($result)) {
-	    echo $row['destino'];
-
+	// Create connection
+	$conn = mysqli_connect($servername, $username, $password, $database);
+	// Check connection
+	if (!$conn) {
+		  die("Connection failed: " . mysqli_connect_error());
 	}
-
+	 
+	echo "Connected successfully";
+	
+	if (mysqli_query($conn, $sql)) {
+		  echo "New record created successfully";
+	} else {
+		  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+	}
+	mysqli_close($conn);
+	?>
 ?>
